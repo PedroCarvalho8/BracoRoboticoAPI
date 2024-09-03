7323b3fe604fda874a26967f04ec0b021ae5ff0a
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-03 19:30:20 -0300

    config: Initial WebSocket config

--- src/main/routes/routes.py
+++ src/main/routes/routes.py
@@ -1,7 +1,13 @@
 from flask import jsonify, Blueprint, request
+from flask_sock import Sock
 
 routes_bp = Blueprint("trips_routes", __name__)
 
 @routes_bp.route("/endpoint", methods=["POST"])
 def route_function():
     return None
+
+@sock.route("/...")
+def websocket_handler():
+    while True:
+        pass
