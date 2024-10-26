ca06bc5f0ddc81c90faedb4c7125204d1126f5fe
Pedro Carvalho <pedrohpcarvalho@outlook.com>
2024-10-26 04:38:30 -0300

    feat: Condigura servidor para recebir requisicoes de qualquer origem

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sock import Sock
 from src.main.routes.routes import game_routes_bp
 
 sock = Sock()
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 sock.init_app(app)
 app.register_blueprint(game_routes_bp)
